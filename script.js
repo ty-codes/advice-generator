63e974088dcec5068490282917b5b328d4bf7a18
Bolatito Adeoye <titoadeoye00@gmail.com>
2022-09-28 13:59:10 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function getAdvice() {
+
+    fetch('https://api.adviceslip.com/advice')
+        .then(res => res.json())
+        .then(data => {
+            document.getElementById('error').innerText = '';
+            document.getElementById('advice').innerText = `"${data.slip.advice}"`
+            document.getElementById('number').innerText = data.slip.id
+        }
+        ).catch(err => {
+            console.log(err);
+            document.getElementById('error').innerText = 'Network problem. Try reconnecting...'
+        }
+        )
+}
+
+    
+
+
+
+
+
